928ff1a3390780577e12656383e7e7278c5ce695
Rockson Teh <rocksondance@gmail.com>
2017-07-22 11:52:50 +0800

    Stair Intersection Done (read desc)
    instructions for intersections:
    press up arrow key or w to go up grounded stairs
    press down arrow key or s to go down stairs

--- Unnamed Unity Project/Assets/Scripts/Controller2D.cs
+++ Unnamed Unity Project/Assets/Scripts/Controller2D.cs
@@ -68,7 +68,7 @@ public class Controller2D : RaycastController
             rayOrigin += Vector2.up * (horizontalRaySpacing * i);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
 
-            Debug.DrawRay(rayOrigin, Vector2.right * directionX , Color.red);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX, Color.red);
 
             if (hit)
             {
@@ -201,7 +201,7 @@ public class Controller2D : RaycastController
     {
         RaycastHit2D maxSlopeHitLeft = Physics2D.Raycast(raycastOrigins.bottomLeft, Vector2.down, Mathf.Abs(moveAmount.y) + skinWidth, collisionMask);
         RaycastHit2D maxSlopeHitRight = Physics2D.Raycast(raycastOrigins.bottomRight, Vector2.down, Mathf.Abs(moveAmount.y) + skinWidth, collisionMask);
-        if(maxSlopeHitLeft ^ maxSlopeHitRight)
+        if (maxSlopeHitLeft ^ maxSlopeHitRight)
         {
             SlideDownMaxSlope(maxSlopeHitLeft, ref moveAmount);
             SlideDownMaxSlope(maxSlopeHitRight, ref moveAmount);
@@ -244,7 +244,7 @@ public class Controller2D : RaycastController
         if (hit)
         {
             float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-            if(slopeAngle > maxSlopeAngle)
+            if (slopeAngle > maxSlopeAngle)
             {
                 moveAmount.x = Mathf.Sign(hit.normal.x) * (Mathf.Abs(moveAmount.y) - hit.distance) / Mathf.Tan(slopeAngle * Mathf.Deg2Rad);
 
---
+++ Unnamed Unity Project/Assets/Scripts/DynamicCollisions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicCollisions : MonoBehaviour {
+
+    public Collider2D groundStair;
+    public bool input;
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            groundStair.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if ((other.tag == "Player" && Input.GetKey(KeyCode.W) && input) || (other.tag == "Player" && Input.GetKey(KeyCode.UpArrow) && input))
+        {
+            groundStair.gameObject.SetActive(true);
+        }
+    }
+}
