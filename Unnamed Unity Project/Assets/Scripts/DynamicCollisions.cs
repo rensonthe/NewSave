4824c762108d2ab31158d0df8589701bf28ad314
Rockson Teh <rocksondance@gmail.com>
2017-07-23 14:15:08 +0800

    OMG SERIOUSLY WHY DIDNT I THINK OF THIS
    end me. that was so freaking stupid

--- Unnamed Unity Project/Assets/Scripts/DynamicCollisions.cs
+++ Unnamed Unity Project/Assets/Scripts/DynamicCollisions.cs
@@ -5,20 +5,29 @@ public class DynamicCollisions : MonoBehaviour {
 
     public Collider2D groundStair;
     public bool input;
+    public bool deactivate;
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            groundStair.gameObject.SetActive(false);
+            groundStair.gameObject.SetActive(true);
         }
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if ((other.tag == "Player" && Input.GetKey(KeyCode.W) && input) || (other.tag == "Player" && Input.GetKey(KeyCode.UpArrow) && input))
+        if ((other.tag == "Player" && Input.GetKey(KeyCode.S) && input) || (other.tag == "Player" && Input.GetKey(KeyCode.DownArrow) && input))
         {
-            groundStair.gameObject.SetActive(true);
+            groundStair.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && deactivate)
+        {
+            groundStair.gameObject.SetActive(false);
         }
     }
 }
